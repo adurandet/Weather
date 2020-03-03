9a35c3e9c081107743eac7697721a4b8e4e7f2ff
Alexandre Durandet <alexandre.durandet@playsport.com>
2020-03-03 13:57:50 +1100

    Alternative using liveData coroutine scope

--- app/src/main/java/com/adurandet/weather/ui/main/viewmodel/SearchRequestHistoryViewModel.kt
+++ app/src/main/java/com/adurandet/weather/ui/main/viewmodel/SearchRequestHistoryViewModel.kt
@@ -3,6 +3,8 @@ package com.adurandet.weather.ui.main.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import android.util.Log
+import androidx.lifecycle.*
 import com.adurandet.weather.model.DataBaseError
 import com.adurandet.weather.model.SearchRequest
 import com.adurandet.weather.repository.*
@@ -19,15 +21,30 @@ class SearchRequestHistoryViewModel(searchRequestHistoryRepository: SearchReques
 
             try {
 
-                val searchRequest = searchRequestHistoryRepository.getSearchRequestHistoryAsync().await()
-
+                val searchRequest =
+                    searchRequestHistoryRepository.getSearchRequestHistoryAsync().await()
                 searchRequestHistory.value = Success(searchRequest)
 
-            }catch (e: Exception){
+            } catch (e: Exception) {
 
                 searchRequestHistory.value = Failure(DataBaseError())
 
             }
+        }
+    }
+
+    val searchRequestHistoryAlternative: LiveData<out Resource<List<SearchRequest>>> = liveData {
+
+        emit(Loading<List<SearchRequest>>())
+
+        try {
+
+            val searchRequests = searchRequestHistoryRepository.getSearchRequestHistoryAsync().await()
+            emit(Success(searchRequests))
+
+        } catch (e: Exception) {
+
+            emit(Failure(DataBaseError()))
 
         }
 
